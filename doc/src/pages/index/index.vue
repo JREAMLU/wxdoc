3e61f8199850da9223b7ded0cc6fac700bc3dd49
jream <ms.08.lu@gmail.com>
2018-08-31 11:43:25 +0800

    feat(add map):

--- doc/src/pages/index/index.vue
+++ doc/src/pages/index/index.vue
@@ -27,7 +27,11 @@
             <div class='show'>省份: {{ipinfo.province}}</div>
             <div class='show'>城市: {{ipinfo.city}}</div>
             <div class='show'>运营商: {{ipinfo.isp}}</div>
+            <div class='show'>经度: {{longitude}}</div>
+            <div class='show'>纬度: {{latitude}}</div>
         </div>
+
+        <map id='map' :longitude="longitude" :latitude="latitude" scale="14" show-location style="width: 90%; height: 300px;"></map>
 
     </div>
 </template>
@@ -46,6 +50,8 @@ export default {
             msg: '',
             ipinfo: {},
             ipshow: '当前所在ip',
+            longitude: '',
+            latitude: '',
         }
     },
 
@@ -56,9 +62,19 @@ export default {
     created () {
         this.getUserInfo();
         this.positionSearch();
+        this.getLoc();
     },
 
     methods: {
+        getLoc() {
+            var setmap = this.setMap;
+            wx.getLocation({
+                type: 'gcj02',
+                success: function(res) {
+                    setmap(res.latitude, res.longitude);
+                }
+            });
+        },
         positionSearch() {
             var ipSearch = this.ipSearch;
 
@@ -115,6 +131,10 @@ export default {
         },
         setIPInfoValue(ipinfo) {
             this.ipinfo = ipinfo;
+        },
+        setMap(lat, long) {
+            this.longitude = long;
+            this.latitude = lat;
         },
         isValidIP(ip) {
             var reg = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/
